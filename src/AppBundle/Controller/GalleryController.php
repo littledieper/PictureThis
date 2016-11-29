2f679f6cc3c2f3054ffdd2fa0ac47aebc14c0e00
Alex <alexdiep96@gmail.com>
2016-11-29 01:36:51 -0800

    Adding to prelim gallery

--- src/AppBundle/Controller/GalleryController.php
+++ src/AppBundle/Controller/GalleryController.php
@@ -13,29 +13,26 @@ class GalleryController extends Controller{
 	 * @Route("/gallery", name="gallery")
 	 */
 	public function showAction(Request $request) {
-		
 		/*
 		 * We'll be making a custom query to grab multiple rows by ID
 		 * so we can randomly display the items...
 		 */
 		$em = $this->getDoctrine()->getManager();
-		
 		// retrieve max number of rows from table images
 		$max = $em->createQuery("select count('id') from AppBundle\Entity\Image")->getSingleScalarResult();
 		
-		// is this the best way to do this?
-		// get paths to images so we can use them in the html
-		$helper = $this->container->get('vich_uploader.templating.helper.uploader_helper');
-		$paths = array();
-		for ($i = 0; $i < 5; $i++) {
+		// retrieve 6 random images
+		$images = array();
+		for ($i = 0; $i < 6; $i++) {
 			$id = rand(0, $max);
-			$image = $this->getDoctrine()->getRepository('AppBundle:Image')->find($id);
-			$paths[] = $helper->asset($image, 'image');
-		}
+			$images[] = $this->getDoctrine()
+			->getRepository('AppBundle:Image')
+			->find($id);
+		} // end for
 		
 		return $this->render('default/gallery.html.twig', array(
-				'paths' => $paths
+				'images' => $images
 		));
-	}
+	} // end showAction
 	
 }
