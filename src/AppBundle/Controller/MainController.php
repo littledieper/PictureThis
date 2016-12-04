55f5a9a113a51e3826af6c618a2c275ce2efdad9
Alex <alexdiep96@gmail.com>
2016-12-04 03:14:57 -0800

    Update homepage

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -14,10 +14,31 @@ class MainController extends Controller {
 	 */
 	public function showAction(Request $request)
 	{
+
+		/*
+		 * We'll be making a custom query to grab multiple rows by ID
+		 * so we can randomly display the items...
+		 */
+		$em = $this->getDoctrine()->getManager();
+		// retrieve max number of rows from table images
+		$max = $em->createQuery("select count('id') from AppBundle\Entity\Image")->getSingleScalarResult();
+		
+		// retrieve 8 random images
+		$images = array();
+		for ($i = 0; $i < 8; $i++) {
+			$id = rand(1, $max);
+			$images[] = $this->getDoctrine()
+			->getRepository('AppBundle:Image')
+			->find($id);
+		} // end for
+	
+		
 		/*
 		 * Render out the homepage.
 		 * See views\default\index.html.twig for HTML
 		 */
-		return $this->render('default/index.html.twig');
+		return $this->render('default/index.html.twig', array(
+				'images' => $images
+		));
 	}
 }
